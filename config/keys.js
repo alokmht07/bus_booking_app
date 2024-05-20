b839c0ccb7e93f62c0619310b299a60cbb70bce4
Alok Mahato <alokmahato861@gmail.com>
2024-05-21 02:30:18 +0530

    implement backend

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+const DB = process.env.DATABASE
+
+module.exports = {
+    MongoURI: "mongodb+srv://alokmht07:sibalok07@cluster0.jvfamur.mongodb.net/bus_booking_app?retryWrites=true&w=majority&appName=Cluster0"
+}
